af28328c08d68102bd88c483dc43f854c64c8cbd
grit0 <krit405f@gmail.com>
2016-04-28 00:00:44 +0700

    Create fixnas.sh

---
+++ fixnas.sh
@@ -0,0 +1,26 @@
+sudo apt-get install ntfs-3g -y
+cd ~
+mkdir usb
+pathgit=~/usb
+sudo mkdir -p $pathgit
+sudo chown -R pi:pi $pathgit
+sudo mount /dev/sda1 $pathgit
+cd $pathgit
+echo "--------------------------------------"
+ls -al
+echo "--------------------------------------"
+sudo bash -c "echo /dev/sda1       $pathgit   auto    uid=pi,gid=pi     0       0 >>/etc/fstab"
+sudo apt-get install samba samba-common-bin -y
+echo
+echo
+sudo bash -c "echo security = user >>/etc/samba/smb.conf"
+sudo bash -c "echo [Git]>>/etc/samba/smb.conf"
+sudo bash -c "echo comment = Git server>>/etc/samba/smb.conf"
+sudo bash -c "echo path = $pathgit>>/etc/samba/smb.conf"
+sudo bash -c "echo valid users = @users>>/etc/samba/smb.conf"
+sudo bash -c "echo force group = users>>/etc/samba/smb.conf"
+sudo bash -c "echo create mask = 0660>>/etc/samba/smb.conf"
+sudo bash -c "echo directory mask = 0771>>/etc/samba/smb.conf"
+sudo bash -c "echo read only = no>>/etc/samba/smb.conf"
+sudo smbpasswd -a pi
+sudo service samba restart
