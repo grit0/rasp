09c4c36dac66ff99329b6ce7e960f2d2794cff5a
grit0 <krit405f@gmail.com>
2016-11-23 22:20:24 +0700

    Update status.sh

--- status.sh
+++ status.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 case "$(python3 --version 2>&1)" in
     **"."[!0-2]*)
         echo "Fine!"
@@ -7,6 +8,16 @@ case "$(python3 --version 2>&1)" in
         echo "Wrong Python version!"
         ;;
 esac
+#sudo pip3 inistall pyrebase
 mkdir ~/status_app
 wget https://raw.githubusercontent.com/grit0/rasp/master/first.py -O ~/status_app/first.py
+
+#cron
+crontab -l > status_cron
+echo "* * * * * python3 ~/status_app/first.py" >> status_cron
+crontab status_cron
+rm status_cron
+
+
+
 python3 ~/status_app/first.py
