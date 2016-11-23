e4e487176eeed76e3edc7eedbe74b42dc9b43f84
grit0 <krit405f@gmail.com>
2016-11-23 21:54:32 +0700

    Update status.sh

--- status.sh
+++ status.sh
@@ -7,5 +7,6 @@ case "$(python3 --version 2>&1)" in
         echo "Wrong Python version!"
         ;;
 esac
+mkdir ~/status_app
 wget https://raw.githubusercontent.com/grit0/rasp/master/first.py -O ~/status_app/first.py
 python3 ~/status_app/first.py
