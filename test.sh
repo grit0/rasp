5232c972dc201c1445379d19c8de82ceb9c3f1fb
grit0 <krit405f@gmail.com>
2017-03-02 15:17:44 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 case "$(python3 --version 2>&1)" in
     **"."[!0-2]*)
         echo "Fine!"
-         mkdir ~/test
+        mkdir ~/test
         ;;
     *)
         echo "Wrong Python version! Not support Firebase"
