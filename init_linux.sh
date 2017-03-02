7babada295dda266c9a624c9af9781f147bc7cbb
grit0 <krit405f@gmail.com>
2017-03-02 21:20:42 +0700

    Update init_linux.sh

--- init_linux.sh
+++ init_linux.sh
@@ -2,7 +2,10 @@
 echo alias in=/'sudo apt-get install /' >> ~/.bashrc
 echo alias ud=/'sudo apt-get update/' >> ~/.bashrc
 echo alias ug=/'sudo apt-get upgrade -y/' >> ~/.bashrc
-source ~/.bashrc
-sudo apt-get update && sudo apt-get -y upgrade
+echo alias sH=/'sudo shutdow -h/' >> ~/.bashrc
+echo alias lcr=/'tail -f /var/log/syslog | grep CRON/' >> ~/.bashrc
+source ~/.bashrc 
+sudo apt-get update && sudo apt-get -y upgrade  
 sudo apt-get install vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
