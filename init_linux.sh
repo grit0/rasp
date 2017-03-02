14acf0ed4532f71b9e227ee7e356e75cd798da42
grit0 <krit405f@gmail.com>
2017-03-02 16:25:46 +0700

    Update init_linux.sh

--- init_linux.sh
+++ init_linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo alias in='sudo apt-get install ' >> ~/.bashrc
-echo alias ud='sudo apt-get update' >> ~/.bashrc
-echo alias ug='sudo apt-get upgrade' >> ~/.bashrc
+echo alias in=/'sudo apt-get install /' >> ~/.bashrc
+echo alias ud=/'sudo apt-get update/' >> ~/.bashrc
+echo alias ug=/'sudo apt-get upgrade -y/' >> ~/.bashrc
 source ~/.bashrc
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install vim
