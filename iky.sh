4dac6626b7142dab6102e62f2928f77e418c2526
grit0 <krit405f@gmail.com>
2017-10-18 17:13:26 +0700

    Update iky.sh
    add hostname to firebase

--- iky.sh
+++ iky.sh
@@ -2,3 +2,4 @@ git clone https://github.com/alfredfrancis/ai-chatbot-framework.git
 cd ai-chatbot-framework
 docker-compose build
 docker-compose up -d
+curl -X POST -d '{"hostname" : "'$(hostname -i)'", "port":"'$(netstat -tulpn | grep proxy |tr -d " " | cut -d ":" -f 4 | tr '\n' ',' | sed 's/.$//')'"}' https://dockerbygrit0.firebaseio.com/.json
