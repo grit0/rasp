bc7f5ace888a7ff00a172ae32bccb5c630d9a62a
grit0 <krit405f@gmail.com>
2017-11-05 23:59:46 +0700

    Create bot_fb.sh

---
+++ bot_fb.sh
@@ -0,0 +1,26 @@
+mkdir ~/bot_fb
+cd ~/bot_fb
+sudo npm install facebook-chat-api
+read -p "What are your ID Facebook? : " id
+read -p "Waht are youre PASSWORD Facebook? : " password
+read -p "What is id who posted message? : " id_des
+echo "ID: $id  +  PASSWORD : $password --> $id_des"
+cat <<EOT > ~/bot_fb/ip.js
+const login = require("facebook-chat-api");
+const ifaces = require('os').networkInterfaces();
+  let address;
+  Object.keys(ifaces).forEach(dev => {
+    ifaces[dev].filter(details => {
+      if (details.family === 'IPv4' && details.internal === false) {
+        address = details.address;
+      }
+    });
+  });
+login({email: "$id", password: "$password"}, (err, api) => {
+    if(err) return console.error(err);
+    const yourID = "$id_des";
+    const msg = address;
+    api.sendMessage(msg, yourID);
+});
+EOT
+echo node ~/bot_fb/ip.js >> ~/.config/lxsession/LXDE-pi/autostart
