8ffba3fac51649b9bd23bd6817463813ccee2661
grit0 <krit405f@gmail.com>
2017-11-06 09:31:11 +0700

    Create read.sh

---
+++ read.sh
@@ -0,0 +1,4 @@
+read -p "What are your ID Facebook? : " id
+read -p "Waht are youre PASSWORD Facebook? : " password
+read -p "What is id who posted message? : " id_des
+echo "ID: $id  +  PASSWORD : $password --> $id_des"
